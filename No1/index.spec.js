771e6df4d7f8b9b119ad694b00bc138e07bfe9e3
dorayo <yomingyo@gmail.com>
2017-09-06 22:56:29 +0800

    remove some rubbish

--- No1/index.spec.js
+++ No1/index.spec.js
@@ -1,5 +1,4 @@
-//测试的话 借鉴群里 需要把 homework.js里的 export 注释掉 ... 感觉很low 暂时只能这样了 尽力了 =。= 
-// 复习去了 ...
+
 describe("第一个测试环境",function () {
     it("测试下自己的函数",function(){
         expect((new PraiseButton).count()).toBe(1);
